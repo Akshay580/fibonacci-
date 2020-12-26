01694bf1fae3fc0804f38955e6d3ecab7cb1191e
Akshay580 <75792864+Akshay580@users.noreply.github.com>
2020-12-26 20:42:22 +0530

    Add files via upload

---
+++ fibonacci.py....py
@@ -0,0 +1,20 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:/python/python3.9.0/fibonacci.py =================
+How many terms? 13
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+>>> 
